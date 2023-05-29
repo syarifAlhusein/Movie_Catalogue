2093dab666384df1964d6d8609fae913045d36b0
syarifAlhusein <muhammadsyarifalhusein@gmail.com>
2023-05-29 18:21:10 +0700

    first commit

---
+++ webpack.dev.js
@@ -0,0 +1,26 @@
+const { merge } = require("webpack-merge");
+const path = require("path");
+const common = require("./webpack.common");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+
+module.exports = merge(common, {
+  mode: "development",
+  devtool: "inline-source-map",
+  devServer: {
+    static: path.resolve(__dirname, "dist"),
+    open: true,
+    port: 9000,
+    client: {
+      overlay: {
+        errors: true,
+        warnings: true,
+      },
+    },
+    compress: true,
+  },
+
+  plugins: [
+    new CleanWebpackPlugin(),
+  ],
+  
+});
